9e862aba17f45749a33bf659c3a69a6bb9ec0650
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-06-27 10:14:48 -0600

    first commit

---
+++ AppleSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleSearch
+//
+//  Created by Timothy Rosenvall on 6/27/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
